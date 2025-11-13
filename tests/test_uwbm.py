2aa40ed1a26017d7bcd57a3c8851d9d2eb0b3139
Luuk Blom <luuk.blom@deltares.nl>
2025-11-13 14:45:00 +0100

    add license, cleanup tests

--- tests/test_uwbm.py
+++ tests/test_uwbm.py
@@ -1,14 +1,14 @@
 """Testing UWBM high level methods."""
 
+from pathlib import Path
+
+import pytest
+
 from hydromt_uwbm import UWBM
 
 
-def test_model_has_name():
-    model = UWBM()
-    assert hasattr(model, "_NAME")
-
-
-def test_model_class():
+@pytest.fixture
+def model() -> UWBM:
     model = UWBM()
     model.setup_project(
         name="test_model",
@@ -17,6 +17,25 @@ def test_model_class():
         t_start="2020-01-01",
         t_end="2020-01-10",
     )
+    return model
 
+
+def test_model_has_name():
+    model = UWBM()
+    assert hasattr(model, "_NAME")
+
+
+def test_model_class(model: UWBM):
     non_compliant = model._test_model_api()
     assert len(non_compliant) == 0, non_compliant
+
+
+def test_write(model: UWBM, tmp_path: Path):
+    model.set_root(root=tmp_path)
+    model.write()
+
+    assert Path(model.root).exists()
+    for sub_dir in model._FOLDERS:
+        assert (Path(model.root) / sub_dir).exists()
+
+    assert Path(model.root, "landuse", f"landuse_{model._NAME}.geojson").exists()
--- tests/test_uwbm_methods.py
+++
@@ -1,5 +0,0 @@
-"""Unit tests for hydromt_plugin_uwbm specific methods eg setup methods or workflows."""
-
-
-def test_random_number_add(random_number, one):
-    assert random_number + one == 5  # should work for all random numbers right?
