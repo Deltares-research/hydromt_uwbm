fefd4532701064d4f56a18397f8adfcd7efd7d7e
Luuk Blom <luuk.blom@deltares.nl>
2025-11-14 14:06:37 +0100

    add model integration test using Daans data

--- tests/test_uwbm.py
+++ tests/test_uwbm.py
@@ -2,26 +2,24 @@
 
 from pathlib import Path
 
-import geopandas as gpd
-import pandas as pd
-
 from hydromt_uwbm import UWBM
 
 DATA_DIR = Path(__file__).parent / "data"
 
 
 def test_model(tmp_path: Path):
-    local_data_catalog = DATA_DIR / "local_data_catalog.yml"
     model = UWBM(
         root=tmp_path,
-        data_libs=["deltares_data", local_data_catalog.as_posix()],
+        data_libs=[
+            "deltares_data",
+            (DATA_DIR / "landuse" / "local_data_catalog.yml").as_posix(),
+        ],
     )
 
-    region_geojson = DATA_DIR / "Shapefile_Area_priority.geojson"
     model.setup_project(
-        name="Logrono",
-        region={"geom": region_geojson},
-        t_start="1990-01-01",
+        name="Athens_Votris",
+        region={"geom": DATA_DIR / "Athens_Votris_region.geojson"},
+        t_start="2020-02-01",
         t_end="2020-03-01",
         ts=3600,
         crs="EPSG:3857",
@@ -32,24 +30,30 @@ def test_model(tmp_path: Path):
     model.setup_landuse(source="osm", landuse_mapping_fn="osm_mapping")
     model.write()
 
-    _assert_equal_model_output(model_root=Path(model.root))
+    _assert_equal_models(left=DATA_DIR / "Athens_Votris_model", right=Path(model.root))
 
 
-def _assert_equal_model_output(model_root: Path):
-    landuse_csv = "landuse_Athens_Votris.csv"
-    assert (model_root / landuse_csv).exists()
-    landuse_df = pd.read_csv(model_root / landuse_csv)
-    expected_landuse_df = pd.read_csv(DATA_DIR / landuse_csv)
-    assert landuse_df.equals(expected_landuse_df)
+def _assert_equal_models(left: Path, right: Path):
+    l_model = UWBM(root=left, mode="r")
+    r_model = UWBM(root=right, mode="r")
 
-    forcing_csv = "Forcing_Logrono_30y_1h.csv"
-    assert (model_root / forcing_csv).exists()
-    forcing_df = pd.read_csv(model_root / forcing_csv)
-    expected_forcing_df = pd.read_csv(DATA_DIR / forcing_csv)
-    assert forcing_df.equals(expected_forcing_df)
+    assert len(l_model.geoms) > 0
+    assert len(l_model.geoms) == len(r_model.geoms)
+    for geom_name in l_model.geoms:
+        l_geom = l_model.geoms[geom_name]
+        r_geom = r_model.geoms[geom_name]
+        assert l_geom.equals(r_geom)
 
-    landuse_geojson = "landuse_Athens_Votris.geojson"
-    assert (model_root / landuse_geojson).exists()
-    landuse_gdf = gpd.read_file(model_root / landuse_geojson)
-    expected_landuse_gdf = gpd.read_file(DATA_DIR / landuse_geojson)
-    assert landuse_gdf.equals(expected_landuse_gdf)
+    assert len(l_model.forcing) > 0
+    assert len(l_model.forcing) == len(r_model.forcing)
+    for f_name in l_model.forcing:
+        l_forcing = l_model.forcing[f_name]
+        r_forcing = r_model.forcing[f_name]
+        assert l_forcing.equals(r_forcing)
+
+    assert len(l_model.tables) > 0
+    assert len(l_model.tables) == len(r_model.tables)
+    for t_name in l_model.tables:
+        l_table = l_model.tables[t_name]
+        r_table = r_model.tables[t_name]
+        assert l_table.equals(r_table)
